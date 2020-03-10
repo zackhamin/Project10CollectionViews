1e91e759320f9f20350ba6e5f095109d0a7406fe
Zack <ishaqamincoop@icloud.com>
2020-03-10 12:04:57 +0000

    Initial commit

---
+++ Project10CollectionViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project10CollectionViews
+//
+//  Created by Ishaq Amin on 10/03/2020.
+//  Copyright © 2020 Ishaq Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
